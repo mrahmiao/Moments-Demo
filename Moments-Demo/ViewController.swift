10cf95c86076a7386d79e92442b3289b2db701f5
mrahmiao <mrahmiao@gmail.com>
2018-04-08 18:28:40 +0800

    feat: 增加 HeaderView
    Signed-off-by: mrahmiao <mrahmiao@gmail.com>

--- Moments-Demo/ViewController.swift
+++ Moments-Demo/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+  @IBOutlet weak var headerView: HeaderView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,27 @@ class ViewController: UITableViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
 
+// MARK: - UITableViewDataSource
+extension ViewController {
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
+}
+
+// MARK: - UITableViewDelegate
+extension ViewController {
+  override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return tableView.frame.width + 50 // 留白
+  }
+
+}
---
+++ Moments-Demo/Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  Moments-Demo
+//
+//  Created by mrahmiao on 4/8/18.
+//  Copyright © 2018 NSTweak. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+
+  @IBOutlet weak var headerImageView: UIImageView!
+  @IBOutlet weak var avatarImageView: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var avatarWidthConstraint: NSLayoutConstraint!
+
+}
