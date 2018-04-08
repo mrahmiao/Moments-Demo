21b8a28f18d2c66460f629e2e663519afca5f009
mrahmiao <mrahmiao@gmail.com>
2018-04-08 14:45:57 +0800

    feat: 增加 User 结构
    Signed-off-by: mrahmiao <mrahmiao@gmail.com>

---
+++ Moments-Demo/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Moments-Demo
+//
+//  Created by mrahmiao on 4/8/18.
+//  Copyright © 2018 NSTweak. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+  let id: String
+  let nickname: String
+}
