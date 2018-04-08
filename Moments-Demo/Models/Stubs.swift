c34ce4f3182161ab699289b9b4de66c3643e8407
mrahmiao <mrahmiao@gmail.com>
2018-04-08 15:39:52 +0800

    feat: 增加 stub 用户
    Signed-off-by: mrahmiao <mrahmiao@gmail.com>

---
+++ Moments-Demo/Models/Stubs.swift
@@ -0,0 +1,22 @@
+//
+//  Stubs.swift
+//  Moments-Demo
+//
+//  Created by mrahmiao on 4/8/18.
+//  Copyright © 2018 NSTweak. All rights reserved.
+//
+
+import Foundation
+
+var users: [User] = [
+  User(id: "001", nickname: "Mashu"),
+  User(id: "002", nickname: "Artoria"),
+  User(id: "005", nickname: "Nero"),
+  User(id: "023", nickname: "Medusa"),
+  User(id: "047", nickname: "Hercules"),
+  User(id: "048", nickname: "Lancelot"),
+  User(id: "060", nickname: "Orion"),
+  User(id: "065", nickname: "Drake"),
+  User(id: "070", nickname: "Scathach"),
+  User(id: "092", nickname: "Shiki")
+]
