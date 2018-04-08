27b60d9ecef8a1c4d695051a25d4041e81ca4808
mrahmiao <mrahmiao@gmail.com>
2018-04-08 14:48:19 +0800

    feat: 增加 Comment 结构
    Signed-off-by: mrahmiao <mrahmiao@gmail.com>

---
+++ Moments-Demo/Models/Comment.swift
@@ -0,0 +1,15 @@
+//
+//  Comment.swift
+//  Moments-Demo
+//
+//  Created by mrahmiao on 4/8/18.
+//  Copyright © 2018 NSTweak. All rights reserved.
+//
+
+import Foundation
+
+struct Comment {
+  let id: String
+  let content: String
+  let userID: String
+}
